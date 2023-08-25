b8ae6158a143bc3e420ced92092e8688d3331b25
Jeffery Noland <104016970+xnolandx@users.noreply.github.com>
2023-08-24 22:23:40 -0400

    navbar still needs work
    navbar still needs work

--- app/components/navbar/navbar.js
+++ app/components/navbar/navbar.js
@@ -1,14 +1,44 @@
+'use client'
+
 import Link from 'next/link'
 import styles from './navbar.module.css'
+import { useEffect, useState } from 'react'
 
 export default function Navbar(){
-    return(
-          <div className={styles.main}>
-            <Link href='/'> Home </Link>
-            <Link href='/mission'> Mission </Link>
-            <Link href='/vision'> Vision </Link>
-            <Link href='/capabilities'> Capabilities </Link>
-            <Link href='/tools'> Tools </Link>
-          </div>
-    )
-  }
+  const [selectedTab, setSelectedTab] = useState('home')
+
+  return(
+    <div className={styles.main}>
+      <Link 
+      href='/' 
+      className={selectedTab === 'home' ? styles.selectedTab : styles.unselectedTab}
+      onClick={() => setSelectedTab('home')}
+      > Home </Link>
+
+      <Link 
+      href='/mission' 
+      className={selectedTab === 'mission' ? styles.selectedTab : styles.unselectedTab}
+      onClick={() => setSelectedTab('mission')}
+      > Mision </Link>
+
+      <Link 
+      href='/vision' 
+      className={selectedTab === 'vision' ? styles.selectedTab : styles.unselectedTab}
+      onClick={() => setSelectedTab('vision')}
+      > Vision </Link>
+
+      <Link 
+      href='/capabilities' 
+      className={selectedTab === 'capabilities' ? styles.selectedTab : styles.unselectedTab}
+      onClick={() => setSelectedTab('capabilities')}
+      > Capabilities </Link>
+
+      <Link 
+      href='/tools' 
+      className={selectedTab === 'tools' ? styles.selectedTab : styles.unselectedTab}
+      onClick={() => setSelectedTab('tools')}
+      > Tools </Link>
+
+    </div>
+)
+}
